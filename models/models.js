1ba55b8b78de7092a7feccb01150c3575a59e0a0
Ted Smiles <ted@smilesfamily.co.uk>
2024-04-17 14:06:39 +0100

    added endpoint to patch votes of a comment

--- models/models.js
+++ models/models.js
@@ -151,6 +151,17 @@ exports.updateArticleVotes = ({ inc_votes }, id) => {
     })
 }
 
+exports.updateCommentVotes = ({ inc_votes }, id) => {
+    return db.query(`UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *`,[inc_votes, id])
+    .then(({ rows }) => {
+        if (rows[0] === undefined) {
+            return Promise.reject({ status: 404, msg: 'comment_id does not exist'})
+        } else {
+            return rows[0]
+        }
+    })
+}
+
 exports.deleteCommentById = (id) => {
     return db.query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *`,[id])
     .then(({ rows }) => {
--- routes/comments-router.js
+++ routes/comments-router.js
@@ -1,9 +1,10 @@
 const commentRouter = require('express').Router()
 
-const { deleteComment } = require("../controllers/controllers")
+const { deleteComment, patchComment } = require("../controllers/controllers")
 
 commentRouter
     .route('/:comment_id')
+    .patch(patchComment)
     .delete(deleteComment)    
 
 module.exports = commentRouter
